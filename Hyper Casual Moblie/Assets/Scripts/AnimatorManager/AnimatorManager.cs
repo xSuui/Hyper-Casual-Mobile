e1ae73436fb1b505fa20f70440caa35dc5cb4e13
xSuui <samuel2012rogerio@gmail.com>
2023-11-01 21:03:52 -0300

    add animator manager

---
+++ Hyper Casual Moblie/Assets/Scripts/AnimatorManager/AnimatorManager.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimatorManager : MonoBehaviour
+{
+    public Animator animator;
+    public List<AnimatorSetup> animatorSetups;
+
+    public enum AnimationType
+    {
+        IDLE,
+        RUN,
+        DEAD
+    }
+
+    public void Play(AnimationType type)
+    {
+        foreach (var animation in animatorSetups)
+        {
+            if (animation.type == type)
+            {
+                animator.SetTrigger(animation.trigger);
+                break;
+            }
+        }
+    }
+
+    public void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            Play(AnimationType.RUN);
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            Play(AnimationType.DEAD);
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            Play(AnimationType.IDLE);
+        }
+    }
+}
+
+[System.Serializable]
+
+public class AnimatorSetup
+{
+    public AnimatorManager.AnimationType type;
+    public string trigger; 
+}
