9035a63a1f9dc830910a8092271608c0a0a2fbb8
xSuui <samuel2012rogerio@gmail.com>
2023-11-02 14:27:14 -0300

    add and config the level manager

---
+++ Hyper Casual Moblie/Assets/Scripts/LevelManager/LevelManager.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+
+    public Transform container;
+
+    public List<GameObject> levels;
+
+    [SerializeField] private int _index;
+    private GameObject _currentLevel;
+
+    private void Awake()
+    {
+        SpawnNextLevel();
+    }
+
+    private void SpawnNextLevel()
+    {
+        if(_currentLevel != null)
+        {
+            Destroy(_currentLevel);
+            _index++;
+            if(_index >= levels.Count)
+            {
+                ResetLevelIndex();
+            }
+        }
+
+        _currentLevel = Instantiate(levels[_index], container);
+        _currentLevel.transform.localPosition = Vector3.zero;
+    }
+
+    private void ResetLevelIndex()
+    {
+        _index = 0;
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.D))
+        {
+            SpawnNextLevel();
+        }
+    }
+}
