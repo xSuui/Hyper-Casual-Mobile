b90ee4563109ba72772b9980320e11ce5dfec7ea
xSuui <samuel2012rogerio@gmail.com>
2023-11-02 15:05:35 -0300

    adding randomization to scene pieces

--- Hyper Casual Moblie/Assets/Scripts/LevelManager/LevelManager.cs
+++ Hyper Casual Moblie/Assets/Scripts/LevelManager/LevelManager.cs
@@ -9,12 +9,31 @@ public class LevelManager : MonoBehaviour
 
     public List<GameObject> levels;
 
+    [Header("Pieces")]
+    public List<LevelPieceBase> levelPiecesStart;
+    public List<LevelPieceBase> levelPieces;
+    public List<LevelPieceBase> levelPiecesEnd;
+
+
+    public int piecesStartNumber = 3;
+    public int piecesNumber = 5;
+    public int piecesEndNumber = 1;
+
+
+    public float timeBetweemPieces = .3f;
+    
+    
     [SerializeField] private int _index;
     private GameObject _currentLevel;
 
+    private List<LevelPieceBase> _spawnedPieces;
+
+
+
     private void Awake()
     {
-        SpawnNextLevel();
+        //SpawnNextLevel();
+        CreateLevelPieces();
     }
 
     private void SpawnNextLevel()
@@ -37,6 +56,56 @@ public class LevelManager : MonoBehaviour
     {
         _index = 0;
     }
+
+    #region
+    private void CreateLevelPieces()
+    {
+
+        _spawnedPieces = new List<LevelPieceBase>();
+
+        for(int i = 0; i < piecesStartNumber; i++)
+        {
+            CreateLevelPiece(levelPiecesStart);
+        }
+
+        for(int i = 0; i < piecesNumber; i++)
+        {
+            CreateLevelPiece(levelPieces);
+        }
+        for(int i = 0; i < piecesEndNumber; i++)
+        {
+            CreateLevelPiece(levelPiecesEnd);
+        }
+
+        //StartCoroutine(CreateLevelPiecesCoroutine());
+    }
+
+    private void CreateLevelPiece(List<LevelPieceBase> list)
+    {
+        var piece = list[Random.Range(0, list.Count)];
+        var spanwedPiece = Instantiate(piece, container);
+
+        if(_spawnedPieces.Count > 0)
+        {
+            var lastPiece = _spawnedPieces[_spawnedPieces.Count - 1];
+
+            spanwedPiece.transform.position = lastPiece.endPiece.position;
+        }
+
+        _spawnedPieces.Add(spanwedPiece);
+    }
+
+    IEnumerator CreateLevelPiecesCoroutine()
+    {
+        _spawnedPieces = new List<LevelPieceBase>();
+
+        for (int i = 0; i < piecesNumber; i++)
+        {
+            CreateLevelPiece(levelPieces);
+            yield return new WaitForSeconds(timeBetweemPieces);
+        }
+    }
+    #endregion
 
     private void Update()
     {
