66f7fe620064535cfcfa85ad839e300d1f50e05e
xSuui <samuel2012rogerio@gmail.com>
2023-11-03 15:02:59 -0300

    adjust bug coin complete

--- Hyper Casual Moblie/Assets/Scripts/CoinsAnimator/CoinsAnimationManager.cs
+++ Hyper Casual Moblie/Assets/Scripts/CoinsAnimator/CoinsAnimationManager.cs
@@ -19,6 +19,9 @@ public class CoinsAnimationManager : Singleton<CoinsAnimationManager>
     private void Start()
     {
         itens = new List<ItemCollectableCoin>();
+
+       
+
     }
 
     public void RegisterCoin(ItemCollectableCoin i)
@@ -68,3 +71,79 @@ public class CoinsAnimationManager : Singleton<CoinsAnimationManager>
     }
     
 }
+
+
+
+#region test de resolução
+/*public class CoinsAnimationManager : Singleton<CoinsAnimationManager>
+{
+    public List<ItemCollectableCoin> itens;
+
+    [Header("Animation")]
+    public float scaleDuration = .2f;
+    public float scaleTimeBetweenPieces = .1f;
+    public Ease ease = Ease.OutBack;
+
+    private void Start()
+    {
+        itens = new List<ItemCollectableCoin>();
+    }
+
+    public void RegisterCoin(ItemCollectableCoin i)
+    {
+        if (!itens.Contains(i))
+        {
+            itens.Add(i);
+            i.transform.localScale = Vector3.zero;
+        }
+    }
+
+    public void ClearCoinsList()
+    {
+        foreach (ItemCollectableCoin coin in itens)
+        {
+            // Destrua ou desative as moedas antigas na lista
+            Destroy(coin.gameObject); // ou coin.gameObject.SetActive(false);
+        }
+        itens.Clear(); // Limpe a lista
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            StartAnimations();
+        }
+    }
+
+    public void StartAnimations()
+    {
+        ClearCoinsList(); // Limpe a lista antes de iniciar as novas animações
+        StartCoroutine(ScalePiecesByTime());
+    }
+
+    IEnumerator ScalePiecesByTime()
+    {
+        foreach (var p in itens)
+        {
+            p.transform.localScale = Vector3.zero;
+        }
+
+        Sort();
+
+        yield return null;
+
+        for (int i = 0; i < itens.Count; i++)
+        {
+            itens[i].transform.DOScale(1, scaleDuration).SetEase(ease);
+            yield return new WaitForSeconds(scaleTimeBetweenPieces);
+        }
+    }
+
+    private void Sort()
+    {
+        itens = itens.OrderBy(
+            x => Vector3.Distance(this.transform.position, x.transform.position)).ToList();
+    }
+}*/
+#endregion
